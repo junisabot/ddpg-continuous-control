2db97d3a38b69195d7983c4cbb4e8550af3da2e8
iamlewis <levischg@gmail.com>
2022-07-23 02:55:30 +0800

    test

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@
 import random
 import copy
 import numpy as np
-from collections import deque
+from collections import namedtuple, deque
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
@@ -17,113 +17,111 @@ random.seed(config.SEED)
 device = torch.device(config.DEVICE)
 
 class DDPG():        
-    def __init__(self, input_dims, action_dims, num_agents):        
-        self.input_dims = input_dims
-        self.action_dims = action_dims
+    def __init__(self, state_size, action_size, num_agents):
+        random_seed = config.SEED
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(random_seed)
         self.num_agents = num_agents
+        self.memory = ReplayBuffer(action_size, config.BUFFER_SIZE, config.BATCH_SIZE, random_seed)
+        
+        self.noise = OUNoise(action_size, random_seed)
+        self.critic_local = Critic(state_size, action_size, random_seed).to(device)
+        self.critic_target = Critic(state_size, action_size, random_seed).to(device)
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=config.LR_CRITIC, weight_decay=0)
 
-        self.actor_local = Actor(self.input_dims, self.action_dims).to(device)
-        self.actor_target = Actor(self.input_dims, self.action_dims).to(device)
-        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=config.LR)
-        self.critic_local = Critic(self.input_dims, self.action_dims).to(device)
-        self.critic_target = Critic(self.input_dims, self.action_dims).to(device)
-        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=config.LR)
+        self.actor_local = Actor(state_size, action_size, random_seed).to(device)
+        self.actor_target = Actor(state_size, action_size, random_seed).to(device)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=config.LR_ACTOR)
 
-        self.noise = OUNoise((self.num_agents, self.action_dims))
-        self.memory = ReplayBuffer(self.input_dims, self.action_dims)
-    
-    def step(self, state, action, reward, next_state, done):
+    def step(self, states, actions, rewards, next_states, dones):
         for i in range(self.num_agents):
-            self.memory.add(state[i,:], action[i,:], reward[i], next_state[i,:], done[i])
-        batch = self.memory.sample()
-        if batch is None:
-            return
-        self.learn(batch)
-
-    def act(self, state, add_noise=True):
-        state = torch.from_numpy(state).float().to(device)
+            self.memory.add(states[i], actions[i], rewards[i], next_states[i], dones[i])
+        if len(self.memory) > config.BATCH_SIZE:
+            experiences = self.memory.sample()
+            self.learn(experiences, config.GAMMA)
+        
+    def act(self, states, add_noise=True):
+        states = torch.from_numpy(states).float().to(device)
+        actions = np.zeros((self.num_agents, self.action_size))
         self.actor_local.eval()
         with torch.no_grad():
-            action = self.actor_local(state).cpu().data.numpy()
-        self.actor_local.train()        
+            for i, state in enumerate(states):
+                actions[i, :] = self.actor_local(state).cpu().data.numpy()
+        self.actor_local.train()
         if add_noise:
-            action += self.noise.sample()
-        return np.clip(action, -1, 1)
+            actions += self.noise.sample()
+        return np.clip(actions, -1, 1)
 
     def reset(self):
         self.noise.reset()
 
-    def learn(self, batch):
-        states, actions, rewards, next_states, dones = batch
+    def learn(self, experiences, gamma):
+        states, actions, rewards, next_states, dones = experiences
 
         actions_next = self.actor_target(next_states)
         Q_targets_next = self.critic_target(next_states, actions_next)
-        Q_targets = rewards + (config.GAMMA * Q_targets_next * (1 - dones))
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
         Q_expected = self.critic_local(states, actions)
-        critic_loss = F.mse_loss(Q_expected, Q_targets)
 
+        critic_loss = F.mse_loss(Q_expected, Q_targets)
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 1)
         self.critic_optimizer.step()
-
+        
         actions_pred = self.actor_local(states)
         actor_loss = -self.critic_local(states, actions_pred).mean()
-
+        
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         self.actor_optimizer.step()
 
-        self.update_model(self.critic_local, self.critic_target)
-        self.update_model(self.actor_local, self.actor_target)
+        self.update_model(self.critic_local, self.critic_target, config.TAU)
+        self.update_model(self.actor_local, self.actor_target, config.TAU)                     
 
-    def update_model(self, local_model, target_model):
-        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(config.TAU*local_param.data + (1.0-config.TAU)*target_param.data)
-
-class OUNoise:
-    def __init__(self, size, mu=0., theta=0.15, sigma=0.2):
-        self.size = size        
-        self.mu = mu * np.ones(size)
-        self.theta = theta
-        self.sigma = sigma        
-        self.reset()
-
-    def reset(self):
-        self.state = copy.copy(self.mu)
-
-    def sample(self):
-        x = self.state        
-        dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(self.size)
-        self.state = x + dx
-        return self.state
+    def update_model(self, regular_model, target_model, tau):
+        for target_param, regular_param in zip(target_model.parameters(),regular_model.parameters()):
+            target_param.data.copy_(tau * regular_param.data + (1.0 - tau) * target_param.data)
 
 class ReplayBuffer:
-    def __init__(self, input_dim, action_dims, mem_size=config.BUFFER_SIZE):
-        self.mem_size = mem_size
-        self.mem_ctr = 0
-        self.state = np.zeros((mem_size, input_dim), dtype=np.float32)
-        self.action = np.zeros((mem_size, action_dims), dtype=np.int32)
-        self.next_state = np.zeros((mem_size, input_dim), dtype=np.float32)
-        self.reward = np.zeros((mem_size, 1), dtype=np.float32)
-        self.done = np.zeros((mem_size, 1), dtype=np.float32)
-
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        self.seed = random.seed(seed)
+        self.memory = deque(maxlen=buffer_size) 
+        self.action_size = action_size
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+    
     def add(self, state, action, reward, next_state, done):
-        index = self.mem_ctr % self.mem_size
-        self.state[index] = state
-        self.action[index] = action
-        self.next_state[index] = next_state
-        self.reward[index] = reward
-        self.done[index] = done
-        self.mem_ctr += 1
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+    
+    def sample(self):
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+        return (states, actions, rewards, next_states, dones)
+
+    def __len__(self):
+        return len(self.memory)  
+    
+class OUNoise:
+    def __init__(self, size, seed, mu=0.0, theta=0.15, sigma=0.2):
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.reset()
 
     def sample(self):
-        if self.mem_ctr < config.BATCH_SIZE:
-            return None
-        max_sample = min(self.mem_size, self.mem_ctr)
-        batch = np.random.choice(max_sample, config.BATCH_SIZE, replace=False)
-        state = torch.from_numpy(self.state[batch]).float().to(device)
-        action = torch.from_numpy(self.action[batch]).float().to(device)
-        reward = torch.from_numpy(self.reward[batch]).float().to(device)
-        next_state = torch.from_numpy(self.next_state[batch]).float().to(device)
-        done = torch.from_numpy(self.done[batch].astype(np.uint8)).float().to(device)
-        return state, action, reward, next_state, done
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.array([np.random.normal(loc=0, scale=1) for _ in range(len(x))])
+        self.state = x + dx
+        return self.state
+    
+    def reset(self):
+        self.state = copy.copy(self.mu)
--- network/actor.py
+++ network/actor.py
@@ -5,27 +5,27 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-torch.manual_seed(config.SEED)
+
+def hidden_init(layer):
+    fan_in = layer.weight.data.size()[0]
+    lim = 1. / np.sqrt(fan_in)
+    return (-lim, lim)
 
 class Actor(nn.Module):
-    def __init__(self, input_dims, action_dims, fc1_dims=512, fc2_dims=256):
+    def __init__(self, state_size, action_size, seed, fc1_units=600, fc2_units=450):
         super(Actor, self).__init__()
-        self.fc1 = nn.Linear(input_dims, fc1_dims)
-        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
-        self.fc3 = nn.Linear(fc2_dims, action_dims)
-        self._init_weight()
-
+        self.seed = torch.manual_seed(seed)
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.reset_parameters()
+        
+    def reset_parameters(self):
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+ 
     def forward(self, state):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return F.tanh(self.fc3(x))
-
-    def _hidden_init(self, layer):
-        fan_in = layer.weight.data.size()[0]
-        lim = 1. / np.sqrt(fan_in)
-        return (-lim, lim)
-
-    def _init_weight(self):
-        self.fc1.weight.data.uniform_(*self._hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*self._hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
